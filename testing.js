86593bf3bfe1ea89da6a4b9e374cb2d7df4a0315
Mackenzie Rempel <mackenziegrempel@gmail.com>
2025-03-15 17:59:19 -0500

    Added drawer drag and drop logic

--- testing.js
+++ testing.js
@@ -1,37 +1,58 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //Add pieces to drawer.
+
+  const drawer = document.getElementById("drawer");
+
+  const piece = new Piece(0, 0, false, true).node;
+  piece.onmousedown = (e) => {
+    console.log("hit");
+    const newPiece = new Piece(0, 0, false);
+    document.getElementById("board").appendChild(newPiece.node);
+    newPiece.startDrag(e);
+  };
+
+  drawer.appendChild(piece);
+});
+
 function addPiece() {
-  const node = new Piece(100, 0);
+  const node = new Piece(100, 0, true);
   document.getElementById("board").appendChild(node.node);
 }
 
 class Piece {
-  constructor(x, y) {
+  constructor(x, y, isStatic = false) {
     this.x = x;
     this.y = y;
-    this.node = document.createElement("div");
 
+    this.node = document.createElement("div");
     this.node.style.width = "50px";
     this.node.style.height = "50px";
-    this.node.style.position = "absolute";
     this.node.style.backgroundColor = "black";
     this.node.draggable = false;
 
-    this.#move(this.x, this.y);
-    this.#initDrag(this.node);
+    this.isStatic = isStatic;
+    if (!isStatic) {
+      this.node.style.position = "absolute";
+      this.#initDrag(this.node);
+      this.#move(this.x, this.y);
+    }
   }
 
   #initDrag = (node) => {
-    node.onmousedown = (e) => {
-      const rect = e.target.getBoundingClientRect();
-      const mouseXDelta = e.clientX - rect.left;
-      const mouseYDelta = e.clientY - rect.top;
-
-      document.onmousemove = (e) => {
-        this.#move(e.clientX - mouseXDelta, e.clientY - mouseYDelta);
-      };
-    };
+    node.onmousedown = (e) => this.startDrag(e);
 
     node.onmouseup = () => {
       document.onmousemove = null;
+    };
+  };
+
+  startDrag = (e) => {
+    const rect = e.target.getBoundingClientRect();
+    const mouseXDelta = e.clientX - rect.left;
+    const mouseYDelta = e.clientY - rect.top;
+
+    document.onmousemove = (e) => {
+      this.#move(e.clientX - mouseXDelta, e.clientY - mouseYDelta);
     };
   };
 
